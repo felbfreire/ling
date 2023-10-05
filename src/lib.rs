293471c42e1767d4fdd981edcb40cb57e0766783
felbfreire <felbfreire@proton.me>
2023-10-05 16:50:48 -0300

    fix lib missing mod, add vec tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod fsystem;
+pub mod ctypes;
 
--- tests/test_ctypes.rs
+++ tests/test_ctypes.rs
@@ -16,3 +16,21 @@ fn test_ctypes_vec() {
     assert_eq!(vec!(1, 2, 5, 4), vector);
     assert_eq!(vec!(1, 2, 5, 4), another_vector);
 }
+
+#[test]
+fn test_ctypes_vec_add_range_for() {
+    let mut vec = ctypes::vector();
+    let mut cap_vec = Vec::with_capacity(6);
+
+    for n in 4..7 {
+        vec.push(n);
+    }
+
+    for n in 1..7 {
+        cap_vec.push(n);
+    }
+
+    assert_eq!(vec!(1, 2, 3, 4, 5, 6), vec);
+    assert_eq!(vec!(1, 2, 3, 4, 5, 6), cap_vec);
+}
+
